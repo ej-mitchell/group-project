1b4713810b3e8861c4bd86e223f5276edcbcbf0a
Eliza Mitchell <eliza.j.mitchell@gmail.com>
2017-07-19 12:53:45 -0400

    Tests/controller/views for Book (index, new, create). Show will be next.

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,28 @@
+class BooksController < ApplicationController
+  def index
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      flash[:notice] = 'Book was successfully added.'
+      redirect_to @book
+    else
+      render action: 'new'
+    end
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :author, :page_number, :summary, :cover_url)
+  end
+
+end
---
+++ spec/features/user_navigates_to_book_index_page_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+=begin
+[] As a user (regardless of authentication), I can navigate from the home page
+to the books index page.
+[] If I am authenticated, I can click on a link to add a book.
+[] If I am not authenticated, clicking the add new book link will send me
+to the sign up page
+=end
+
+feature 'user navigates books index page' do
+  scenario 'user navigates from home index page to books index page', pending: true do
+    visit root_path #this is not set up yet in the master
+    click_link 'Books'
+
+    expect(page).to have('Book List')
+    expect(page).to have_link('Add Book')
+  end
+
+  scenario 'an *authenticated* user clicks on add-book link' do
+    visit books_path
+    click_link 'Add Book'
+
+    expect(page).to have_content('Add New Book Form')
+  end
+
+  scenario 'an *unauthenticated* user clicks on add-book link', pending:true do
+    visit books_path
+    click_link 'Add Book'
+
+    expect(page).to have_content('Sign Up')
+  end
+
+end

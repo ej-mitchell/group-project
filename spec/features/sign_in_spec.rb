1f17d32d93aebee988120ae02afcff39e27c0857
Daniel Park <dpark189@gmail.com>
2017-07-19 15:00:59 -0400

    added sad tests to user authentication

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -26,8 +26,34 @@ feature "sign in" do
       expect(page).to_not have_content('Signed in successfully.')
       expect(page).to_not have_content('Sign Out')
     end
-    scenario 'a existing email with the wrong password is denied'
-    scenario 'an already authenticated user cannot re-sign in'
 
 
+    scenario 'an existing email with the wrong password is denied access' do
+       user = FactoryGirl.create(:user)
+       visit root_path
+       click_link 'Sign In'
+
+       fill_in 'Email', with: user.email
+       fill_in 'Password', with: 'incorrectPassword'
+       click_button 'Log in'
+
+       expect(page).to have_content('Invalid Email or password')
+       expect(page).to_not have_content('Welcome Back!')
+       expect(page).to_not have_content('Sign Out')
+     end
+
+     scenario 'an already authenticated user cannot re-sign in' do
+       user = FactoryGirl.create(:user)
+       visit new_user_session_path
+
+       fill_in 'Email', with: user.email
+       fill_in 'Password', with: user.password
+       click_button 'Log in'
+
+       expect(page).to have_content('Sign Out')
+       expect(page).to_not have_content('Sign In')
+
+       visit new_user_session_path
+       expect(page).to have_content('You are already signed in.')
+     end
   end

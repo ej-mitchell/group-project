805ff9f816d36147e0685a5a8d3fe8347297aed2
Andrew Francisque <andrew.francisque@gmail.com>
2017-07-19 15:57:23 -0400

    add carrierwave functionality

--- db/migrate/20170719174841_add_profile_photo_to_users.rb
+++ db/migrate/20170719174841_add_profile_photo_to_users.rb
@@ -1,5 +1,5 @@
 class AddProfilePhotoToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :profile_photo, :string
+    add_column :users, :profile_photo, :string, null: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 20170719174841) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "profile_photo"
+    t.string "profile_photo", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

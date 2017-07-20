64bd011ede1ae2000bcf39c61fbed2a9eb018c4d
Andrew Francisque <andrew.francisque@gmail.com>
2017-07-20 11:30:05 -0400

    add default photo ability

--- app/uploaders/profile_photo_uploader.rb
+++ app/uploaders/profile_photo_uploader.rb
@@ -11,4 +11,8 @@ class ProfilePhotoUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def default_url(*args)
+    "http://amreading.s3.amazonaws.com/wp-content/uploads/forgetful-fish-dory_559b363bd577b4951.jpg"
+  end
+
 end

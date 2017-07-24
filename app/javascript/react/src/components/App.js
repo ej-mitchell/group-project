26d82fd2340c4550e16fd8fc319458b28df98a85
Eliza Mitchell <eliza.j.mitchell@gmail.com>
2017-07-24 11:00:18 -0400

    added the home page to the router

--- app/javascript/react/src/components/App.js
+++ app/javascript/react/src/components/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route, IndexRoute, Router, browserHistory } from 'react-router';
 import BookIndexContainer from '../containers/BookIndexContainer';
-import BookShowContainer from '../containers/BookShowContainer'
+import BookShowContainer from '../containers/BookShowContainer';
+import HomeIndexContainer from '../containers/HomeIndexContainer';
 
 const App = props => {
   return(
     <div>
       <h1>Welcome to our site!</h1>
       <Router history={browserHistory}>
+        <Route path="/" component={HomeIndexContainer}/>
         <Route path='/books' component={BookIndexContainer}/>
         <Route path='/books/new'/>
         <Route path='/books/:id' component={BookShowContainer}/>
